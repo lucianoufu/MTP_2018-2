f2a18eb3a62713912fdc7b4d6cd007368268800c
lucianoufu <39105165+lucianoufu@users.noreply.github.com>
2018-09-03 23:14:19 -0300

    Update p0.c

--- p0.c
+++ p0.c
@@ -6,7 +6,7 @@ int main(int argc, char ** argv) {
 	int i;
 	if(argc > 1) {
 		if(argc > 2)
-			for(i = 0; i < argv[2][0]-’0’; i++)
+			for(i = 0; i < argv[2][0]-'0'; i++)
 				printf("Valeu, %s\n", argv[1]);
 	else
 		printf("Saudacoes, %s\n", argv[1]);
