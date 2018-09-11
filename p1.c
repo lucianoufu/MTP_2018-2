3aa1707370c4adb8bfa02041e0ca3402db7f6c21
lucianoufu <39105165+lucianoufu@users.noreply.github.com>
2018-09-11 17:29:48 -0300

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+void binario()
+{
+	int estado = 0, i;
+	char bits[256];
+	printf("digite um numero em binario : ");
+	scanf("%s", bits);
+	for(i=0; bits [i] != '\0';i++)
+	{
+		if(estado == 0 && bits[i] == '0'){
+			estado = 0;
+		}
+		else if(estado == 0 && bits[i] == '1'){
+			estado = 1;
+		}
+		else if(estado ==  1 && bits[i] == '0'){
+			estado = 2;
+		}
+		else if(estado == 1 && bits [i] == '1'){
+			estado = 0;
+		}
+		else if(estado == 2 && bits[i] == '0'){
+			estado = 1;
+		}
+		else if(estado == 2 && bits[i] == '1'){
+			estado = 2;
+		}
+	}
+	if(estado == 0)
+		printf(" \"%s\" e multiplo de 3 ", bits);
+	else 
+		printf(" \"%s\" n%co %c\n", bits, 198,130);
+}
+
+int main()
+{
+	int estado = 0, i;
+	char bits[256];
+	printf("digite um numero em binario : ");
+	scanf("%s", bits);
+	for(i=0; bits [i] != '\0';i++)
+	{
+		if(estado == 0 && bits[i] == '0'){
+			estado = 0;
+		}
+		else if(estado == 0 && bits[i] == '1'){
+			estado = 1;
+		}
+		else if(estado ==  1 && bits[i] == '0'){
+			estado = 2;
+		}
+		else if(estado == 1 && bits [i] == '1'){
+			estado = 0;
+		}
+		else if(estado == 2 && bits[i] == '0'){
+			estado = 1;
+		}
+		else if(estado == 2 && bits[i] == '1'){
+			estado = 2;
+		}
+	}
+	if(estado == 0)
+		printf(" \"%s\" e multiplo de 3 ", bits);
+	else 
+		printf(" \"%s\" n%co %c\n", bits, 198,130);
+		
+	return 0;
+}
